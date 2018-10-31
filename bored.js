31a3316bbb9be6b5a2fd2648e86e5e92bebd4867
achyoav <yoavshaby6203@gmail.com>
2018-10-31 21:28:08 +0200

    r

--- bored.js
+++ bored.js
@@ -4,7 +4,7 @@ function aciticiti () {
      'Watch a TV show from the nineties', 'Watch a silent film','Learn a new skill online',
      'Look at your baby pictures','Have a nice day','Compliment a friend','Read a random Wikipedia article',
      'Fully listen to a music album','Go to the library and pick up a random book','Play with action figures'
-    ,'Watch a nature documentary','Watch and old Disney movie','Take a walk','Talk with you parent/s'
+    ,'Watch a nature documentary','Watch and old Disney movie','Take a walk','Talk with your parent/s'
     ,'Eat','Cook','Draw a dragon','Learn how to draw a kangaroo','Watch an old viral video','Call your grandparents.'
     ,'Fix something','Just do it'];    
     var rand = myArray[Math.floor(Math.random() * myArray.length)];
